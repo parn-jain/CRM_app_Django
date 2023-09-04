e037dec874bd00729454a754a503f2e03b2cb325
“parn” <pjisvgreat@gmail.com>
2023-09-05 01:54:35 +0530

    basic app completed , next is authentication

--- crm/website/views.py
+++ crm/website/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 def home(request):
-    return render(request,'home.html',{})
+    return render(request,'index.html')
+    # return HttpResponse('this is home page')
+def about(request):
+    # return HttpResponse("this is about page")
+    return render(request,'about.html')
+
+def customer(request):
+    return HttpResponse("this is customer page") 
 # Create your views here.
