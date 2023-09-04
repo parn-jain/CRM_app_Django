2a31325f8ed7ebe7a67b96030a119e2a736e0f28
“parn” <pjisvgreat@gmail.com>
2023-09-05 04:55:32 +0530

    login and logout features added

--- crm/website/urls.py
+++ crm/website/urls.py
@@ -5,5 +5,8 @@ from. import views
 urlpatterns = [
     path('',views.home,name = 'home'),
     path('about', views.about,name = 'about'),
-    path('customer',views.customer, name = 'customer')
+    path('customer',views.customer, name = 'customer'),
+    path('login',views.login_user, name = 'login'),
+    path('logout',views.logout_user, name = 'logout')
+
 ]
--- crm/website/views.py
+++ crm/website/views.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse, HttpResponse, redirect
+from django.contrib.auth import authenticate,login, logout
+from django.contrib import messages
 def home(request):
-    return render(request,'index.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        #authenticate
+        user = authenticate(request,username = username, password = password)
+        if user is not None:
+            login(request,user)
+            messages.success(request, "You have successfully logined")
+            return redirect('home')
+        else:
+            messages.success(request, "there is some error")
+            return redirect('home')
+    else:
+        return render(request,'index.html')
     # return HttpResponse('this is home page')
 def about(request):
     # return HttpResponse("this is about page")
     return render(request,'about.html')
 
 def customer(request):
-    return HttpResponse("this is customer page") 
-# Create your views here.
+    return HttpResponse("this is customer page")
+def login_user(request):
+    pass
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You hanve succrssfully loged out")
+    return redirect('home')
